28aed1b5557566420edf38bb4dba9aabaaec9a01
felipe.rodriguez.fonte <felipe.rodriguez.fonte@gmail.com>
2023-04-22 22:36:10 +0200

    Delete displays

--- crypto/mac/x99_mac.go
+++ crypto/mac/x99_mac.go
@@ -14,9 +14,6 @@ func GenerateMacX99(inMacData []byte, keyData []byte) ([]byte, error) {
 	macData := make([]byte, len(inMacData))
 	copy(macData, inMacData)
 
-	println(hex.EncodeToString(inMacData), hex.EncodeToString(macData))
-	println(len(macData))
-
 	//add 0 padding
 	if len(macData) < 8 || len(macData)%8 != 0 {
 		pads := make([]byte, 8-(len(macData)%8))
